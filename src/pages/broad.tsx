7a5871c78a80cb3d01ebf431123b0766858233ac
jungle379 <jungle379@gmail.com>
2021-10-19 21:05:15 +0900

    ⚡️ map関数の実装

--- src/pages/broad.tsx
+++ src/pages/broad.tsx
@@ -1,8 +1,36 @@
 // import Header from "../components/Header";
 import Head from "next/head";
-// import Link from "next/link";
+import Link from "next/link";
 
-const broadIndex = () =>
+const ITEMS = [
+   {
+    href: "/",
+    title: "第4回エンジビアの泉",
+    date: "2021年9月8日",
+    status: "放送中",
+    count: "エンジビア数"
+  },{
+    href: "/",
+    title: "第3回エンジビアの泉",
+    date: "2021年8月18日",
+    status: "",
+    count: "エンジビア数"
+  }, {
+    href: "/",
+    title: "第2回エンジビアの泉",
+    date: "2021年7月12日",
+    status: "",
+    count: "エンジビア数"
+  }, {
+    href: "/",
+    title: "第1回エンジビアの泉",
+    date: "2021年5月24日",
+    status: "",
+    count: "エンジビア数"
+  }
+];
+
+const broadcastIndex = () =>
 {
   return <>
   <Head>
@@ -10,9 +38,29 @@ const broadIndex = () =>
     </Head>
     <div className="bg-gray-200 h-screen">
       {/* <Header /> */}
-        <div className="text-4xl w-1/2 mx-auto">放送一覧</div>
+        <div className="text-4xl w-3/5 mx-auto mb-5">放送一覧</div>
+        <div className="bg-white w-3/5 mx-auto">
+        <div>
+        {ITEMS.map(item => {
+          return (
+            <div className="flex border border-gray-100">
+            <div className="h-24">
+              <Link href={item.href}>
+                <h1 className="text-blue-400 ml-5 mt-5">{item.title}</h1>
+              </Link>
+              <h2 className="text-gray-400 ml-5 mt-3">{item.date}</h2>
+            </div>
+            <div className="">
+              <h3 className="mt-5">{item.status}</h3>
+              <h4 className="text-gray-400 mt-3">{item.count}</h4>
+            </div>
+            </div>
+          )
+        })}
+      </div>
+        </div>
     </div>
   </>
 };
 
-export default broadIndex;
+export default broadcastIndex;
