8a6279c5fb52af7f53962966f35f52475463417f
Sota529 <za446131@gmail.com>
2021-10-23 00:41:57 +0900

    ログイン画面作成

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,44 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import { Header } from "components/Header";
 
-const Login = () => {
+const Login: NextPage = () => {
   return (
-    <div className="flex justify-content-between">
+    <div className="flex">
       <div className="w-5/12 flex justify-center items-center">
-        Login
+        <div className="text-center">
+          <div className="block">
+            <Image
+              src="/login/login_logo.png"
+              alt="泉"
+              width="56px"
+              height="60px"
+            />
+          </div>
+          <div className="block mt-6">
+            <Image
+              src="/login/login_title.png"
+              alt="泉"
+              width="206px"
+              height="36px"
+            />
+          </div>
+          <div className="mt-2">
+            <Image
+              src="/login/login_description.png"
+              alt="泉"
+              width="258px"
+              height="20px"
+            />
+          </div>
+          <div className="mt-10 cursor-pointer">
+            <Image
+              src="/login/Signin.png"
+              alt="泉"
+              width="215px"
+              height="50px"
+            />
+          </div>
+        </div>
       </div>
       <figure className="w-7/12 h-screen relative">
         <Image
@@ -18,7 +49,7 @@ const Login = () => {
         />
       </figure>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
