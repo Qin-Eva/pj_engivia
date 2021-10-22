86b96db7166cd3076d73b2d6452255dbe977e391
yugo-ibuki <yugo.ibuki@asia-quest.jp>
2021-10-22 22:36:35 +0900

    タイポ修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,27 +8,27 @@ type Props = {
 
 export const Button: FC<Props> = (({ children, type, onClick }) => {
   const buttonType = () => {
-    let classNme = '';
+    let className = '';
     switch (type) {
       case 'primary':
-        classNme = 'bg-blue-500 rounded-sm py-2 px-5 text-white';
+        className = 'bg-blue-500 rounded-sm py-2 px-5 text-white';
         break;
       case 'secondary':
-        classNme = 'bg-blue-200 rounded-sm py-2 px-5 text-blue-700';
+        className = 'bg-blue-200 rounded-sm py-2 px-5 text-blue-700';
         break;
       case 'onAir':
-        classNme = 'bg-green-100 text-green-600 rounded-full px-5 m-2 text-sm';
+        className = 'bg-green-100 text-green-600 rounded-full px-5 m-2 text-sm';
         break;
       case 'afterOnAir':
-        classNme =
+        className =
           'bg-gray-200　text-gray-400 rounded-full py-1 px-5 m-2 text-sm';
         break;
       case 'beforeOnAir':
-        classNme =
+        className =
           'bg-yellow-300 text-yellow-700 rounded-full py-1 px-5 m-2 text-sm';
     }
 
-    return classNme;
+    return className;
   };
 
   return (
