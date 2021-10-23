5d3ab930b42fea9a17035890f6c0257bdaf50df5
Sota529 <za446131@gmail.com>
2021-10-24 03:13:14 +0900

    TypeScriptに変更

---
+++ src/components/dnd/container.tsx
@@ -0,0 +1,36 @@
+import React, { VFC } from 'react'
+import { useDroppable } from '@dnd-kit/core'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
+
+import { SortableItem } from './sortable_item'
+
+interface Props {
+  id: string
+  items: string[]
+  title: string
+}
+
+export const Container: VFC<Props> = ({ id, items, title }) => {
+  const { setNodeRef } = useDroppable({
+    id,
+  })
+
+  return (
+    <div className="bg-gray-100 p-2 m-2 flex-1">
+      <h3 className="bg-gray-300 h-12 flex justify-center items-center rounded-md">
+        {title}
+      </h3>
+      <SortableContext
+        id={id}
+        items={items}
+        strategy={verticalListSortingStrategy}
+      >
+        <div ref={setNodeRef}>
+          {items.map((id) => (
+            <SortableItem key={id} id={id} />
+          ))}
+        </div>
+      </SortableContext>
+    </div>
+  )
+}
---
+++ src/components/dnd/sortable_item.tsx
@@ -0,0 +1,43 @@
+import React, { VFC } from 'react'
+import Image from 'next/image'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
+interface Props {
+  id: string
+}
+
+export const Item: VFC<Props> = ({ id }) => {
+  return (
+    <div className="w-full items-center justify-center my-2.5 bg-white rounded-md p-4">
+      <p className="text-base">
+        id: {id} HTMLにはポータルという便利な要素がある
+      </p>
+      <div className="mt-4 text-sm flex justify-start items-center">
+        <Image
+          src="/Avatar.png"
+          alt="ユーザー写真"
+          width="24px"
+          height="24px"
+        ></Image>
+        <span className="ml-2">松平 ケン</span>
+      </div>
+    </div>
+  )
+}
+
+export const SortableItem: VFC<Props> = ({ id }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <Item id={id} />
+    </div>
+  )
+}
