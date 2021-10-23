7901a74c421d475232e4480e75a2770a580c7cfc
sanaevvv <sanaevvv@gmail.com>
2021-10-23 11:36:03 +0900

    仕様に沿って一覧にソートを追加してみました

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -5,14 +5,20 @@ type PostType = {
   id: number;
   title: string;
   body: string;
+  created_at: number;
 };
 
 // 一覧取得データ
+// ソートは日付の降順（すなわち新しい日付の放送が上）
 export const getAllPostsData = async () => {
   const res = await fetch(apiUrl);
   const posts = await res.json();
-  return posts;
-}
+  const filteredPosts = posts.sort(
+    (a: PostType, b: PostType) =>
+      b.created_at - a.created_at
+  );
+  return filteredPosts;
+};
 
 // データのIDを一覧取得
 export const getAllPostIds = async () => {
@@ -26,11 +32,11 @@ export const getAllPostIds = async () => {
       },
     };
   });
-}
+};
 
 // IDから特定のデータを取得
 export const getPostData = async (id: string) => {
   const res = await fetch(`${apiUrl}/${id}/`);
   const post = await res.json();
   return post;
-}
+};
