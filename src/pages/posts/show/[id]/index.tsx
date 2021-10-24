0541b2314f5805b5951751c6c8f39539d66359af
arai motoki <arai@nal.vn>
2021-10-25 03:01:34 +0900

    #10 投稿待機ページを作成いたしました

---
+++ src/pages/posts/show/[id]/index.tsx
@@ -0,0 +1,16 @@
+import { StandBy } from 'components/StandBy'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const PostStandBy: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>投稿詳細ページ</title>
+      </Head>
+      <StandBy />
+    </>
+  )
+}
+
+export default PostStandBy
