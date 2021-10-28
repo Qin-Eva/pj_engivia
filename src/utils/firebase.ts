837dfca825f4eb25d6768e38ba8c86ff6ffcd4ae
Sota529 <za446131@gmail.com>
2021-10-28 16:52:22 +0900

    ログイン時にユーザーテーブルに追加する。

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,15 @@
-import * as firebase from "firebase/app";
-import { getAuth, signInWithPopup, GithubAuthProvider } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import * as firebase from 'firebase/app'
+import { getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth'
+import {
+  collection,
+  Timestamp,
+  getFirestore,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+} from 'firebase/firestore'
 
 export const config = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -14,30 +23,44 @@ export const config = {
 // !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 const app = firebase.initializeApp(config)
 
-// export const auth = firebase.auth();
-// export const Firebase = firebase;
+const db = getFirestore(app)
+export const auth = getAuth(app)
 
-const db = getDatabase(app);
-const auth = getAuth(app);
-
+interface authResponse {
+  created_at: Timestamp
+  id: number
+  name: string
+  role_id: number
+}
 export const LoginWithGithub = () => {
-  const provider = new GithubAuthProvider();
+  const provider = new GithubAuthProvider()
   signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    const credential = GithubAuthProvider.credentialFromResult(result);
-    const token = credential?.accessToken;
-
-    const user = result.user;
-    console.log(user);
-    location.assign("/");
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.email;
-    const credential = GithubAuthProvider.credentialFromError(error);
-  });
-};
+    .then(async (result) => {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      const credential = GithubAuthProvider.credentialFromResult(result)
+      const token = credential?.accessToken
+      const docSnap = await getDocs(
+        query(collection(db, 'users'), orderBy('created_at', 'desc'), limit(1))
+      )
+      let id: number = 0 // 初期化
+      docSnap.forEach((doc) => {
+        id = doc.data().id + 1
+      })
+      await addDoc(collection(db, 'users'), {
+        created_at: Timestamp.fromDate(new Date()),
+        id: id,
+        name: result.user.displayName,
+        role_id: id,
+      })
+      location.assign('/')
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      const email = error.email
+      const credential = GithubAuthProvider.credentialFromError(error)
+    })
+}
 
 // ログイン状態の検知
 export const listenAuthState = (dispatch: any) => {
@@ -45,28 +68,28 @@ export const listenAuthState = (dispatch: any) => {
     if (user) {
       // User is signed in.
       dispatch({
-        type: "login",
+        type: 'login',
         payload: {
           user,
         },
-      });
+      })
     } else {
       // User is signed out.
       // ...
       dispatch({
-        type: "logout",
-      });
+        type: 'logout',
+      })
     }
-  });
-};
+  })
+}
 
 export const firebaseUser = () => {
-  return firebase.auth().currentUser;
-};
+  return firebase.auth().currentUser
+}
 
 // Logout
 export const Logout = () => {
   auth.signOut().then(() => {
-    window.location.reload();
-  });
-};
+    window.location.reload()
+  })
+}
