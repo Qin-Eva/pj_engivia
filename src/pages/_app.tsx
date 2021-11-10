e3fd4803bcb2c6d0e91503ff4b41ef05b4e677be
kijikunnn <unicast.trial.b@unicast.ne.jp>
2021-11-10 10:36:32 +0900

    RecoilRoot導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,18 @@ import type { AppProps } from 'next/app'
 import { NextPage } from 'next'
 import Layout from 'components/Layout'
 import { useRouter } from 'next/router'
+import { RecoilRoot } from 'recoil'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter()
   return (
     <>
       {router.pathname !== '/login' ? (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
       ) : (
         <Component {...pageProps} />
       )}
