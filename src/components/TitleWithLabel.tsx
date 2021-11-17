f22fb5b035fec031b05001b7d5c2f92cec15e98c
kijikunnn <unicast.trial.b@unicast.ne.jp>
2021-11-17 09:36:35 +0900

    statusのswitch文にdefaultを追加，margin-bottomを追加

--- src/components/TitleWithLabel.tsx
+++ src/components/TitleWithLabel.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { useMemo, VFC } from 'react'
 
 type Props = {
@@ -23,15 +24,16 @@ export const TitleWithLabel: VFC<Props> = (props) => {
           text: '放送済み',
           color: 'bg-gray-200 text-gray-900'
         }
+      default:
     }
   }, [props.is_streamed])
 
   return (
-    <div className="text-center">
+    <div className="mb-8 text-center">
       <div className={`inline text-sm rounded-full py-1 px-3 ${status?.color}`}>
         {status?.text}
       </div>
-      <h1 className="text-gray-900 font-bold text-3xl mt-4">{props.title}</h1>
+      <h1 className="mt-4 text-3xl font-bold text-gray-900">{props.title}</h1>
     </div>
   )
 }
