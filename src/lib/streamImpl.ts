4bc7d12bed48d5f40c7910f62fbe7d62c5d2de4b
yugo-ibuki <y.ibuki91@gmail.com>
2021-11-19 01:28:14 +0900

    日付の問題解消

--- src/lib/streamImpl.ts
+++ src/lib/streamImpl.ts
@@ -1,13 +1,14 @@
 import { collection, addDoc, getDocs, query } from 'firebase/firestore'
 import { db } from 'utils/firebase'
-import { QuerySnapshot, DocumentData } from '@firebase/firestore'
+import { QuerySnapshot, DocumentData, Timestamp } from '@firebase/firestore'
 
 export type TStream = {
   hee_count: number
   is_streamed: number
   title: string
-  created_at: string
-  updated_at: string
+  stream_date: Timestamp
+  created_at: Timestamp
+  updated_at: Timestamp
 }
 
 export const getStreams = async (): Promise<QuerySnapshot<DocumentData>> => {
