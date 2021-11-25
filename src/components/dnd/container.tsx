0a3a031ede9444f231505abcee483d69a495c910
kijikunnn <kijikunnn@gmail.com>
2021-11-25 15:59:41 +0900

    pathnameが放送中("/admin/posts")ならばNullItemを出す

--- src/components/dnd/container.tsx
+++ src/components/dnd/container.tsx
@@ -1,18 +1,24 @@
 import React, { VFC } from 'react'
+import { useRouter } from 'next/router'
 import { useDroppable } from '@dnd-kit/core'
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
-
 import { SortableItem } from './sortable_item'
+import { NullItem } from './NullItemCard'
 
 type Props = {
   id: string
-  items: string[]
+  items: Array<{ docId: string; docContent: string; docFeatured: string }>
   title: string
 }
 
 export const Container: VFC<Props> = ({ id, items, title }) => {
+  const router = useRouter()
   const { setNodeRef } = useDroppable({
     id
+  })
+
+  const contents: Array<string | { id: string }> = items.map((item) => {
+    return item.docContent
   })
 
   return (
@@ -22,13 +28,15 @@ export const Container: VFC<Props> = ({ id, items, title }) => {
       </h3>
       <SortableContext
         id={id}
-        items={items}
+        items={contents}
         strategy={verticalListSortingStrategy}
       >
         <div ref={setNodeRef}>
-          {items.map((id) => (
-            <SortableItem key={id} id={id} />
+          {items?.map((item) => (
+            <SortableItem key={item.docId} id={item.docContent} />
           ))}
+
+          {router.pathname === '/admin/posts' && <NullItem title={title} />}
         </div>
       </SortableContext>
     </div>
