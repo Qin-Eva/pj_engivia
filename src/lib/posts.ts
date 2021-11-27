506d77d9d3a890a518ae6c4856bc9fd8d6873f29
kijikunnn <kijikunnn@gmail.com>
2021-11-27 20:34:48 +0900

    不要コードの削除

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,48 +1,6 @@
 import { addDoc, collection } from '@firebase/firestore'
 import { db } from 'utils/firebase'
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/posts'
-
-type PostType = {
-  uid: number
-  id: number
-  title: string
-  body: string
-  created_at: number
-}
-
-// 一覧取得データ
-// ソートは日付の降順（すなわち新しい日付の放送が上）
-export const getAllPostsData = async (): Promise<any> => {
-  const res = await fetch(apiUrl)
-  const posts = await res.json()
-  const filteredPosts = posts.sort(
-    (a: PostType, b: PostType) => b.created_at - a.created_at
-  )
-  return filteredPosts
-}
-
-// データのIDを一覧取得
-export const getAllPostIds = async (): Promise<any> => {
-  const res = await fetch(apiUrl)
-  const posts = await res.json()
-
-  return posts.map((post: PostType) => {
-    return {
-      params: {
-        id: String(post.id)
-      }
-    }
-  })
-}
-
-// IDから特定のデータを取得
-export const getPostData = async (id: string): Promise<any> => {
-  const res = await fetch(`${apiUrl}/${id}/`)
-  const post = await res.json()
-  return post
-}
-
 export const createPostData = async (
   content: string,
   user: string
