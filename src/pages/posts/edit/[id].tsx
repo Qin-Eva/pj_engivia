c2fca7a1e3320574ab23a54ffca230be528db2a4
tudukake <info@tudukake.com>
2021-12-05 15:05:36 +0900

    投稿編集ページ

--- src/pages/posts/edit/[id].tsx
+++ src/pages/posts/edit/[id].tsx
@@ -1,27 +1,69 @@
 import Head from 'next/head'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NextPage } from 'next'
 import { TitleWithLabel } from 'components/TitleWithLabel'
 import Button from 'components/Button'
 import RecoilProvider from 'components/RecoilProvider'
+import { db, firebaseUser } from 'utils/firebase'
+import { getPost } from 'lib/posts'
+import router, { useRouter } from 'next/router'
+import { doc, DocumentData, Timestamp, updateDoc } from '@firebase/firestore'
+import { getStream } from 'lib/streamImpl'
 
 const PostEdit: NextPage = () => {
-  const [text, setText] = useState<string>(
-    'HTMLにはポータルという便利な要素がある'
-  )
+  const [text, setText] = useState<string>()
+  const streamDocId = useRouter().query.id as string
+  const [stream, setStream] = useState<DocumentData>()
+
+  const savePost = async (): Promise<void> => {
+    const userDocId = firebaseUser()?.uid
+    const postData = await getPost(streamDocId, userDocId)
+    const postRef = doc(db, 'posts', postData.id)
+    await updateDoc(postRef, {
+      content: text,
+      updated_at: Timestamp.now(),
+      updated_by: userDocId
+    })
+    void router.push('/broadcasts')
+  }
+
+  const handleCancel = (): void => {
+    void router.push('/posts/detail/' + streamDocId)
+  }
+
+  // 放送の一覧取得
+  useEffect(() => {
+    async function f(): Promise<void> {
+      const userDocId = firebaseUser()?.uid
+      const postData = await getPost(streamDocId, userDocId)
+      if (postData != null) {
+        setText(postData.data().content)
+      } else {
+        void router.push('/posts/create')
+      }
+
+      const data = await getStream(streamDocId)
+      setStream(data)
+    }
+    void f()
+  }, [streamDocId])
+
   return (
     <>
       <Head>
         <title>投稿編集ページ</title>
       </Head>
       <div className="mx-auto w-[700px]">
-        <TitleWithLabel title="第4回エンジビアの泉" is_streamed={1} />
+        <TitleWithLabel
+          title={stream?.title}
+          is_streamed={stream?.is_streamed}
+        />
         <form className="block mt-[32px] w-full bg-white">
           <textarea
-            className="block textarea"
+            className="block text-[36px] textarea"
             placeholder="エンジビアを入力する"
             value={text}
-            rows={4}
+            rows={2}
             onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
               setText(e.target.value)
             }
@@ -29,12 +71,12 @@ const PostEdit: NextPage = () => {
         </form>
         <div className="flex justify-center mt-[32px]">
           <div className="mr-[32px]">
-            <Button type="primary" onClick={() => console.log('click')}>
+            <Button type="primary" onClick={savePost}>
               保存する
             </Button>
           </div>
           <div>
-            <Button type="secondary" onClick={() => console.log('click')}>
+            <Button type="secondary" onClick={handleCancel}>
               キャンセル
             </Button>
           </div>
